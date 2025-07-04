a214607332ee32e0f558fdf23c68452846e716f8
soyinkanaomi <soyinkanaomi@gmai.com>
2025-07-04 13:48:24 +0100

    intor to typescript

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var myName = "naomi";
+console.log(myName);
+var food = "rice and beans";
+console.log(food);
+var age = 30;
+console.log(age);
+var subject = ["mathmatics", "english", "yoruba"];
+console.log(subject);
+var Numarray = [4, 5, 6, 7];
+console.log(Numarray);
+var allArray = [11, "nine", true];
+console.log(allArray);
+function multiply(a, b) {
+    return a - b;
+}
+console.log(multiply(4, 4));
---
+++ index.ts
@@ -0,0 +1,23 @@
+let myName:string = "naomi"
+console.log(myName)
+
+let food:string = "rice and beans"
+ console.log(food)
+
+ let age:number = 30
+ console.log(age)
+
+ let subject:Array<string>=["mathmatics", "english", "yoruba"]
+ console.log(subject)
+
+ let Numarray:number[] =[4, 5, 6, 7]
+ console.log(Numarray)
+
+ let allArray:Array<Number | String | Boolean> = [11, "nine", true]
+ console.log(allArray)
+
+ function multiply(a: number, b: number){
+return a-b
+ }console.log(multiply(4,4))
+
+ export{}
